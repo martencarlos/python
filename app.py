619d43ab3e0f6b1910df197ead387942b2c66450
Carlos Marten <martencarlos@gmail.com>
2023-04-14 15:59:42 +0200

    render test

--- app.py
+++ app.py
@@ -20,40 +20,40 @@ modelYolo = YOLO("yolov8n-cls.pt")  # load a pretrained model (recommended for t
 # modelResnet18.eval()
 
 # # Define the image preprocessing function
-def preprocess_image_resnet18(image_url):
+# def preprocess_image_resnet18(image_url):
 
-    ## Preprocess the image ##
-    # Load the image from the URL
-    image = Image.open(requests.get(image_url, stream=True).raw)
-    print(image)
-    # Define the image preprocessing steps
-    preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
-    ])
-    # Preprocess the image
-    image_tensor = preprocess(image)
-    image_tensor = image_tensor.unsqueeze(0)
+#     ## Preprocess the image ##
+#     # Load the image from the URL
+#     image = Image.open(requests.get(image_url, stream=True).raw)
+#     print(image)
+#     # Define the image preprocessing steps
+#     preprocess = transforms.Compose([
+#         transforms.Resize(256),
+#         transforms.CenterCrop(224),
+#         transforms.ToTensor(),
+#         transforms.Normalize(mean=[0.485, 0.456, 0.406],
+#                              std=[0.229, 0.224, 0.225])
+#     ])
+#     # Preprocess the image
+#     image_tensor = preprocess(image)
+#     image_tensor = image_tensor.unsqueeze(0)
     
-    # Use the pre-trained model to detect objects in the image
-    with torch.no_grad():
-        output = modelResnet18(image_tensor)
-        probabilities = torch.nn.functional.softmax(output[0], dim=0)
-        _, indices = torch.sort(output, descending=True)
-        top_indices = indices[0][:5]
+#     # Use the pre-trained model to detect objects in the image
+#     with torch.no_grad():
+#         output = modelResnet18(image_tensor)
+#         probabilities = torch.nn.functional.softmax(output[0], dim=0)
+#         _, indices = torch.sort(output, descending=True)
+#         top_indices = indices[0][:5]
     
-    # Get the labels for the detected objects
-    labels_path = 'https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt'
-    labels = []
-    with urllib.request.urlopen(labels_path) as f:
-        classes = [line.decode('utf-8').strip() for line in f.readlines()]
-    for i in top_indices:
-        labels.append(classes[i])
+#     # Get the labels for the detected objects
+#     labels_path = 'https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt'
+#     labels = []
+#     with urllib.request.urlopen(labels_path) as f:
+#         classes = [line.decode('utf-8').strip() for line in f.readlines()]
+#     for i in top_indices:
+#         labels.append(classes[i])
     
-    return (labels[0])
+#     return (labels[0])
 
 def preprocess_image_yolov5s(image_url,ext):
     response = requests.get(image_url)
